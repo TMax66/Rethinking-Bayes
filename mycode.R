cf2b510cf024a5a200eb3fffc679b78d2293b142
TMax66 <massimo.tranquillo@gmail.com>
2018-11-21 14:38:42 +0100

    up

--- mycode.R
+++ mycode.R
@@ -11,33 +11,53 @@ dt<-tibble(group=c(rep("Control", 5), rep("Treatment",5)), y=c(721,882.88,890.98
 dt<-dt %>% data.frame()
 
 z<-dt %>% 
-  ggplot(aes(x=group, y=y))+geom_boxplot(fill="firebrick4")+
+  ggplot(aes(x=group, y=y))+geom_boxplot(fill="firebrick4")+labs(x="")+
   geom_jitter(aes(), alpha=0.9, 
               position=position_jitter(w=0.1,h=0.1))+coord_flip()
 
 
 fit<-lm(y~group, data=dt)
-coef <- tidy(fit, conf.int = TRUE)
-
-x<-ggplot(coef[2,], aes(term, estimate))+
-  geom_point()+labs(x="")+
+fit%>% 
+tidy(conf.int = TRUE) %>%
+  filter(term=="groupTreatment") %>% 
+  ggplot(aes(term, estimate))+
+  geom_point()+labs(x="", y="effect size")+
   geom_pointrange(aes(ymin = conf.low, ymax = conf.high))+
-  coord_flip()
+  coord_flip()#+scale_y_continuous(limits=c(-10,50))
 
 plot_grid(x,z,ncol = 1, align = 'v')
 
-library(rethinking)
-flist <- alist(
-  y ~ dnorm( mu , sigma ) ,
-  mu ~ dunif( 0,2000 ) ,
-  sigma ~ dunif( 0,500 )
-)
-slist <- list(
-  mu=mean(dt$y),
-  sigma=sd(dt$y)
-)
+plot_grid((dt %>% 
+            ggplot(aes(x=group, y=y))+geom_boxplot(fill="firebrick4")+labs(x="")+
+            geom_jitter(aes(), alpha=0.9, 
+                        position=position_jitter(w=0.1,h=0.1))+coord_flip()),
+          (fit%>% 
+            tidy(conf.int = TRUE) %>%
+            filter(term=="groupTreatment") %>% 
+            ggplot(aes(term, estimate))+
+            geom_point()+labs(x="", y="effect size")+
+            geom_pointrange(aes(ymin = conf.low, ymax = conf.high))+
+            coord_flip()), ncol=1,align='v')
 
-bayes <- map( flist , data=newdt , start=slist)
+
+
+###############################################
+#################BAYES#########################
+###############################################
+
+
+
+
+
+
+
+dt<-tibble(group=c(rep("Control", 8), rep("Treatment",8)), 
+    y=c(4.691,9.952,9.602,7.273,7.808,8.882,7.688,5.303,
+        4.443,11.451,8.732,4.174,6.138,7.485,7.057,8.517))
+dt<-dt %>% data.frame()
+
+ggplot(dt, aes(x=group, y=y))+geom_boxplot()
+
 
 library(brms)
 
